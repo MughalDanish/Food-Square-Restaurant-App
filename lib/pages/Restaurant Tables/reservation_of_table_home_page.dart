99b78089cddf2da5d0eaf6137650b2548205e058
Muhammad Danish <dnmughal54321@gmail.com>
2023-11-24 03:31:25 +0500

    feature/reservation of tables/testing pending

---
+++ lib/pages/Restaurant Tables/reservation_of_table_home_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'TableCart.dart';
+
+class reservation_of_tables_catagory extends StatelessWidget {
+  reservation_of_tables_catagory({super.key});
+  final searchTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0XFFF5F5F5),
+          leading: IconButton(
+            icon: Image(image: AssetImage('assets/images/menuType2.png'), height: 25, width: 20),
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Image(image: AssetImage('assets/images/shopping-cart.png'), height: 20, width: 20),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right:50),
+              child: Text(
+                'Dine with Us, Your Table,\nYour Story',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  fontFamily: 'Inter',
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 40,
+              margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextField(
+                cursorColor: Colors.black54,
+                controller: searchTextController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: 'Search your dining table',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 11),
+                ),
+                onChanged: (text) {},
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                tabs: [
+                  Tab(text: 'Top Rated',),
+                  Tab(text: 'Most Popular'),
+                  Tab(text: 'Special Places'),
+                ],
+                labelPadding: EdgeInsets.symmetric(horizontal: 10), // Adjust padding as needed
+                labelColor: Color(0xFFFF5733), // Set the color of the selected tab label
+                unselectedLabelColor: Colors.black54, // Set the color of unselected tab labels
+                indicatorColor: Color(0xFFFF5733), // Set the color of the tab indicator
+                indicatorWeight: 3, // Set the thickness of the tab indicator
+                indicatorSize: TabBarIndicatorSize.label,
+                //indicatorPadding: EdgeInsets.symmetric(horizontal: 1), // Adjust padding as needed
+                labelStyle: TextStyle(fontSize: 16), // Adjust font size as needed
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  TableCartsGrid(),
+                  Center(child: Text('Drinks Content')),
+                  Center(child: Text('Snacks Content')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
