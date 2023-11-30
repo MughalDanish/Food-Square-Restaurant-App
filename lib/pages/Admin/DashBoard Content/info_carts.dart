aa283d035ebebd328086a69d831ad030ca2a8fac
Muhammad Danish <dnmughal54321@gmail.com>
2023-11-30 08:09:17 +0500

    feature/information cart of total sales/testing complete

--- lib/pages/Admin/DashBoard Content/info_carts.dart
+++ lib/pages/Admin/DashBoard Content/info_carts.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
 
+// InformationCarts Widget
 class InformationCarts extends StatelessWidget {
-  const InformationCarts({super.key});
+  const InformationCarts({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(top:30, left:25),
+        padding: EdgeInsets.only(top: 30, left: 25),
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
             children: [
-              CartDecoration(image: 'assets/images/sales.png', name: 'Total Sales', price: 'Rs, 2999999.909', boxColor: Colors.pinkAccent),
-              SizedBox(width:80),
-              CartDecoration(image: 'assets/images/sales.png', name: 'Total Sales', price: 'Rs, 2999999.909', boxColor: Color(0xFFFF5733)),
-              SizedBox(width:80),
-              CartDecoration(image: 'assets/images/sales.png', name: 'Total Sales', price: 'Rs, 2999999.909', boxColor: Color(0XFF0038FF)),
-              SizedBox(width:80),
-              CartDecoration(image: 'assets/images/sales.png', name: 'Total Sales', price: 'Rs, 2999999.909', boxColor: Colors.deepPurpleAccent),
+              // Total Sales Cart
+              CartDecoration(
+                image: 'assets/images/sales.png',
+                name: 'Total Sales',
+                price: 'Rs, 2999999.909',
+                boxColor: Colors.pinkAccent,
+              ),
+              SizedBox(width: 80),
+              // Total Sales Cart
+              CartDecoration(
+                image: 'assets/images/sales.png',
+                name: 'Total Sales',
+                price: 'Rs, 2999999.909',
+                boxColor: Color(0xFFFF5733),
+              ),
+              SizedBox(width: 80),
+              // Total Sales Cart
+              CartDecoration(
+                image: 'assets/images/sales.png',
+                name: 'Total Sales',
+                price: 'Rs, 2999999.909',
+                boxColor: Color(0xFF0038FF),
+              ),
+              SizedBox(width: 80),
+              // Total Sales Cart
+              CartDecoration(
+                image: 'assets/images/sales.png',
+                name: 'Total Sales',
+                price: 'Rs, 2999999.909',
+                boxColor: Colors.deepPurpleAccent,
+              ),
             ],
           ),
         ),
@@ -26,29 +51,32 @@ class InformationCarts extends StatelessWidget {
     );
   }
 
-  CartDecoration({
+  // CartDecoration Widget
+  Widget CartDecoration({
     required String name,
     required String image,
     required String price,
     required Color boxColor,
   }) {
-    return  Container(
+    return Container(
       height: 100,
       width: 250,
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(7)
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(7),
       ),
       child: Padding(
-        padding: EdgeInsets.only(left:15),
+        padding: EdgeInsets.only(left: 15),
         child: Row(
           children: [
+            // Circular Image Container
             Container(
               height: 60,
               width: 60,
               decoration: BoxDecoration(
-                  color: boxColor,
-                  shape: BoxShape.circle),
+                color: boxColor,
+                shape: BoxShape.circle,
+              ),
               child: Center(
                 child: Image(
                   image: AssetImage(image),
@@ -62,23 +90,29 @@ class InformationCarts extends StatelessWidget {
               padding: EdgeInsets.only(top: 25, left: 10),
               child: Column(
                 children: [
+                  // Cart Name
                   Padding(
-                    padding: EdgeInsets.only(right:50),
-                    child: Text(name,
+                    padding: EdgeInsets.only(right: 50),
+                    child: Text(
+                      name,
                       style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black54,
-                          fontFamily: 'Inter'),
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black54,
+                        fontFamily: 'Inter',
+                      ),
                     ),
                   ),
                   SizedBox(height: 3),
-                  Text(price,
+                  // Cart Price
+                  Text(
+                    price,
                     style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontFamily: 'Inter'),
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontFamily: 'Inter',
+                    ),
                   ),
                 ],
               ),
